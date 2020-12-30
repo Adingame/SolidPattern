8a4f72c4e51c96cc973066af486a0d842160f1b0
Adingame <45685592+Adingame@users.noreply.github.com>
2020-12-30 21:53:42 +0600

    SOLID

---
+++ HomeWork/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main()
+        {
+            //S - single responsibility principle (Этот класс умеет только то что должен уметь)
+            Cat myCat = new Cat();
+            myCat.Name = "Simon";
+            myCat.Eat();
+            myCat.Move();
+
+            //O - Open Closed Principle (При добавление нового функционала мы не переопределяем методы Cat а добавляем новые методы)
+            //При добавлений нового функционала нам будет достаточно добавить этот метод внутри нового класса
+
+            myCat.Eat();
+            WildCat wildCat = new WildCat();
+            wildCat.WildKilling();
+
+
+            //L - Liskov Substitution Principle (Мы можем дополнять но не замещать поведение базового класса)
+            //то есть новый клиентский класс будет добавлять новый функционал в себя но не будет изменять то что умеет Cat
+            Cat bigCat = new WildCat();
+            bigCat.Eat();
+
+
+            //I - Interface Segregation Principle (Клиент не должен зависеть от методов которые не используются!)
+            //Вроде WildDog должен наследовать интерфейс IDog но он не сможет использовать метод Training();
+            //Вместо того что бы наследоватся от IDog и играть с костылями в виде Exception мы просто реализуем отдельный интерфейс с нужными методами
+            //и наследуемся от него
+            WildDog wolf = new WildDog();
+            wolf.Hunt();
+
+
+            //D - Dependency Injection Principle(Нужно абстрагироватся и лишать жёстких связей)
+            //Возможно пример притянут за уши но ничего дельного придумать не смог...
+            DeterminingTypeDog myDog = new DeterminingTypeDog(new FighterDog());
+            
+        }
+    }
+}
---
+++ HomeWork/Solid.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork
+{
+    public abstract class Animal {
+        public string Name { get; set; }
+    }
+
+    public class Cat : Animal, ICat
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Cat eat");
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("Cat move");
+        }
+    }
+
+    public class Dog : Animal, IDog
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Dog eat");
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("Dog move");
+        }
+
+        public void Training()
+        {
+            Console.WriteLine("Training true");
+        }
+    }
+
+    public class WildDog : IWildDog
+    {
+        private void Eat()
+        {
+            Console.WriteLine("Wolf eat");
+        }
+
+        public void Hunt()
+        {
+            Console.WriteLine("Wolf hunting");
+            Eat();
+        }
+    }
+
+    public class WildCat : Cat
+    {
+        public void WildKilling()
+        {
+            Console.WriteLine("Kill animal");
+        }
+    }
+
+    public interface ICat
+    {
+        public void Move();
+        public void Eat();
+    }
+
+    public interface IDog
+    {
+        public void Move();
+
+        public void Eat();
+
+        public void Training();
+    }
+
+    public interface IWildDog
+    {
+        public void Hunt();
+    }
+
+    public class DeterminingTypeDog
+    {
+        IDog dogType;
+        public DeterminingTypeDog(IDog dog)
+        {
+            dogType = dog;
+        }
+    }
+
+    public class FighterDog : IDog
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Training()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ICompanionDog : IDog
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Training()
+        {
+            throw new NotImplementedException();
+        }
+    };
+}
